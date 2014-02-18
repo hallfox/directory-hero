8746706feecbfb00cab562318501670822797424
Taylor Foxhall <lxmachina@gmail.com>
2014-02-18 17:56:06 -0500

    add map manager

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.treeview import TreeView, TreeViewNode, TreeViewLabel
+from kivy.uix.button import Button
 
 from kivy.config import Config
 
 from kivy.app import App
+
+from maps import MapManager
+
+def populate_tree_view(tree_view, parent, node):
+    if parent is None:
+        tree_node = tree_view.add_node(TreeViewLabel(text=node['node_id'],
+                                                     is_open=True))
+    else:
+        tree_node = tree_view.add_node(TreeViewLabel(text=node['node_id'],
+                                                     is_open=True), parent)
+
+    for child_node in node['children']:
+        populate_tree_view(tree_view, tree_node, child_node)
+
+
+class Folder(TreeView, TreeViewNode):
+	pass
+	
+class File(Button, TreeViewNode):
+	pass
 
 class DirectoryHeroApp(App):
 	def build(self):
@@ -13,9 +34,7 @@ class DirectoryHeroApp(App):
 		tree = TreeView()
 		tree.bind(minimum_height=tree.setter('height'))
 		
-		#add some test nodes to the tree
-		tree.add_node(TreeViewLabel(text="Folder 1"))
-		tree.add_node(TreeViewLabel(text="Folder2"))		
+		populate_tree_view(tree, None, MapManager.Tutorial)	
 		
 		view = ScrollView()
 		view.add_widget(tree)
