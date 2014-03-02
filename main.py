0d7f65b0ad0c6f355beb6b59381e252626d8a0fb
Taylor Foxhall <lxmachina@gmail.com>
2014-03-02 17:42:05 -0500

    add kv lang/dictonary map scheme

--- main.py
+++ main.py
@@ -1,45 +1,47 @@
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.treeview import TreeView, TreeViewNode, TreeViewLabel
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 from kivy.config import Config
 
 from kivy.app import App
 
-from maps import MapManager
+from maps import Maps
 
-def populate_tree_view(tree_view, parent, node):
-    if parent is None:
-        tree_node = tree_view.add_node(TreeViewLabel(text=node['node_id'],
-                                                     is_open=True))
-    else:
-        tree_node = tree_view.add_node(TreeViewLabel(text=node['node_id'],
-                                                     is_open=True), parent)
+from collections import OrderedDict
 
-    for child_node in node['children']:
-        populate_tree_view(tree_view, tree_node, child_node)
-
-
-class Folder(TreeView, TreeViewNode):
+class Dir(Label, TreeViewNode):
 	pass
 	
 class File(Button, TreeViewNode):
+	def display_file(self):
+		popup = Popup(title=self.text,
+						content=Label(text=self.data),
+						size_hint=(.5,.5))
+		popup.open()
+	
+class Tutorial(TreeView):
 	pass
-
+	
 class DirectoryHeroApp(App):
 	def build(self):
 		self.title = "Directory Hero"
 		
 		#create the directory tree
-		tree = TreeView()
+		tree = Tutorial()
 		tree.bind(minimum_height=tree.setter('height'))
 		
-		populate_tree_view(tree, None, MapManager.Tutorial)	
+		ids = OrderedDict(sorted(tree.ids.items(), key=lambda t: t[0]))
 		
-		view = ScrollView()
+		for key,node in ids.items():
+			tree.add_node(node, node.parent)
+		
+		view = ScrollView(do_scroll_x=False)
 		view.add_widget(tree)
 		
-		return view		
+		return tree	
 
 if __name__ == "__main__":
 	DirectoryHeroApp().run()
--- maps.py
+++ maps.py
@@ -1,15 +1,37 @@
+"""Map properties:
+id - string:"#.#.#..."
+name - string:will be visible to player
+children - list:contains children
+type - string?:contains information about whether it's a file or a dir"""
 
-class MapManager():	
-	#tutorial map
-	Tutorial = {'node_id': '1',
-			'children':[{'node_id': '1.1',
-						'children':[{'node_id':'1.1.1',
+_tutorial = {'id':'1',
+			'name':'Open me.',
+			'type':'dir',
+			'children':[{'id': '1.1',
+						'name':'A directory.',
+						'type':'dir',
+						'children':[{'id':'1.1.1',
+									'name':'Nothing here.',
+									'type':'file',
+									'content':"nothing",
 									'children':[]},
-									{'node_id':'1.1.2',
+									{'id':'1.1.2',
+									'name':'Look at me!',
+									'type':'file',
+									'content':"look",
 									'children':[]},
-									{'node_id':'1.1.3',
+									{'id':'1.1.3',
+									'name':"You can't see me",
+									'type':'hidden',
 									'children':[]}]},
-						{'node_id': '1.2',
+						{'id': '1.2',
+						'name':'I\'m and empty directory',
+						'type':'dir',
 						'children':[]},
-						{'node_id': '1.3',
+						{'id': '1.3',
+						'name':"I'm a file.",
+						'type':'file',
+						'content':'magic',
 						'children':[]}]}
+						
+Maps = {'Tutorial':_tutorial}
